9d1ca6f3737e1c033150bf55b84d90210600e201
Prinshu <prinshu2003@gmail.com>
2024-03-03 19:17:24 +0530

    completed profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  return (
+    <div className="mx-auto max-w-lg p-3">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4" onSubmit={submitHandler}>
+        <img
+          src={currentUser.avatar}
+          alt="profile-pic"
+          className="rounded-full object-cover h-20 w-20 self-center cursor-pointer"
+        ></img>
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="p-3 rounded-lg border"
+          onChange={handleChange}
+          value={formData.username}
+        ></input>
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="p-3 rounded-lg border"
+          onChange={handleChange}
+          value={formData.email}
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="p-3 rounded-lg border"
+          onChange={handleChange}
+          value={formData.password}
+        ></input>
+        <button
+          type="submit"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+      {error && <p className="mt-5 text-red-700">{error}</p>}
+    </div>
+  );
 };
 
 export default Profile;
