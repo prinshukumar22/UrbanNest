90d3600b4856aaf346a46d182c29fb4aa4e6bc18
Prinshu <prinshu2003@gmail.com>
2024-03-18 00:16:23 +0530

    updated listing card

--- frontend/src/components/ListingCard.jsx
+++ frontend/src/components/ListingCard.jsx
@@ -8,7 +8,7 @@ const ListingCard = ({ listing }) => {
         <img
           src={listing.imageUrls[0]}
           alt="listing_cover"
-          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+          className="h-[320px] sm:h-[220px] w-auto object-cover hover:scale-105 transition-scale duration-300"
         ></img>
         <div className="p-3 flex flex-col gap-2 w-full">
           <p className="truncate text-lg font-semibold text-slate-700">
