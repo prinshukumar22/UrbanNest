706b24b7cc5a8b7057220ad2875b7bb0c14efb5f
Prinshu <prinshu2003@gmail.com>
2024-03-20 22:26:54 +0530

    Implemented lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,45 @@
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Profile from "./pages/Profile";
-import About from "./pages/About";
-import Home from "./pages/Home";
+const SignIn = React.lazy(() => import("./pages/SignIn"));
+const SignUp = React.lazy(() => import("./pages/SignUp"));
+const Profile = React.lazy(() => import("./pages/Profile"));
+const About = React.lazy(() => import("./pages/About"));
 import Header from "./components/Header";
-import PrivateRoute from "./components/PrivateRoute";
-import CreateListing from "./pages/CreateListing";
-import EditListing from "./pages/EditListing";
-import Listing from "./pages/Listing";
-import Search from "./pages/Search";
+const PrivateRoute = React.lazy(() => import("./components/PrivateRoute"));
+const CreateListing = React.lazy(() => import("./pages/CreateListing"));
+const EditListing = React.lazy(() => import("./pages/EditListing"));
+const Listing = React.lazy(() => import("./pages/Listing"));
+const Search = React.lazy(() => import("./pages/Search"));
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/listing/:listingId" element={<Listing />} />
-        <Route path="/search" element={<Search />} />
-        <Route element={<PrivateRoute></PrivateRoute>}>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/createListing" element={<CreateListing />}></Route>
-          <Route
-            path="/editlisting/:listingId"
-            element={<EditListing />}
-          ></Route>
-        </Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <p className="text-2xl text-semibold text-center my-3 text-slate-700">
+            Loading...
+          </p>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/listing/:listingId" element={<Listing />} />
+          <Route path="/search" element={<Search />} />
+          <Route element={<PrivateRoute></PrivateRoute>}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/createListing" element={<CreateListing />}></Route>
+            <Route
+              path="/editlisting/:listingId"
+              element={<EditListing />}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
